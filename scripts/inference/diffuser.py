4085944ddf3c62ff555e08f5cb48e388b703ee71
Công Đinh <dinhchicongf9@gmail.com>
2024-02-29 10:24:10 +0700

    update crawl

---
+++ scripts/inference/diffuser.py
@@ -0,0 +1,42 @@
+import torch
+from diffusers import AutoencoderTiny, StableDiffusionPipeline
+
+from streamdiffusion import StreamDiffusion
+from streamdiffusion.image_utils import postprocess_image
+
+pipe = StableDiffusionPipeline.from_pretrained("SG161222/RealVisXL_V3.0").to(
+    device=torch.device("cuda"),
+    dtype=torch.float16,
+)
+
+stream = StreamDiffusion(
+    pipe,
+    t_index_list=[0, 16, 32, 45],
+    torch_dtype=torch.float16,
+    cfg_type="none",
+)
+
+# If the loaded model is not LCM, merge LCM
+stream.load_lcm_lora()
+stream.fuse_lora()
+# Use Tiny VAE for further acceleration
+stream.vae = AutoencoderTiny.from_pretrained("madebyollin/taesd").to(device=pipe.device, dtype=pipe.dtype)
+# Enable acceleration
+pipe.enable_xformers_memory_efficient_attention()
+
+
+prompt = "1girl with dog hair, thick frame glasses"
+# Prepare the stream
+stream.prepare(prompt)
+
+# Warmup >= len(t_index_list) x frame_buffer_size
+for _ in range(4):
+    stream()
+
+# Run the stream infinitely
+while True:
+    x_output = stream.txt2img()
+    postprocess_image(x_output, output_type="pil")[0].show()
+    input_response = input("Press Enter to continue or type 'stop' to exit: ")
+    if input_response == "stop":
+        break
